f16b5bd0818eb02334549b24a01210dbc3de243d
ricardozv <ricardosilvanet03@gmail.com>
2020-09-16 16:16:30 -0300

    up

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+
+<html>
+        <head>
+                <title> Exemplo </title>
+        </head>
+
+
+        <body>
+
+                <?php
+                        echo "<p> Fala compas</p>";
+                        echo "<p> Fala compas</p>";
+                        echo $_SERVER['HTTP_USER_AGENT'];
+                ?>
+
+
+
+        </body>
+
+
+
+
+
+</html>
