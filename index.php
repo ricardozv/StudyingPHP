fa64643458032a6849406e08c869ee19ad2d818f
ricardozv <ricardosilvanet03@gmail.com>
2020-09-17 00:52:46 -0300

    uploadsPHP

--- index.php
+++ index.php
@@ -2,24 +2,39 @@
 
 <html>
         <head>
+                <meta charset ="UTF-8">
                 <title> Exemplo </title>
         </head>
 
 
         <body>
+                <header class="cabecalho">
+                    <h1>PHP</h1>
+                </header>
 
-                <?php
-                        echo "<p> Fala compas</p>";
-                        echo "<p> Fala compas</p>";
-                        echo $_SERVER['HTTP_USER_AGENT'];
-                ?>
-
-
-
+                <main class="principal">
+                    <div class="conteudo">
+                    </div>
+                </main>
+       
+ 
+        
         </body>
 
-
-
-
-
 </html>
+
+
+
+<?php if ($expression == false): ?>
+  Isto irá aparecer se a expressão for Falça.
+<?php else: ?>
+  Senão isso que aparecerá.
+<?php endif; ?>
+
+<?php
+    echo 'Isto é um teste';
+?>
+
+<?php echo 'Isto é um teste' ?>
+
+<?php echo 'Nós omitimos a última tag de fechamento';
